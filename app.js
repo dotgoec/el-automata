0531712dd25450a7372317bd803f8c65bf9d98a4
Joe X! <jamedinao@gmail.com>
2023-07-23 02:13:53 -0500

    First tests

--- app.js
+++ app.js
@@ -1,3 +1,29 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+import dotenv from 'dotenv'
+dotenv.config();
+import { ChatGPTUnofficialProxyAPI } from 'chatgpt'
+import express from 'express';
+import { Server } from "socket.io";
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("hello");
+});
+
+app.listen(process.env.PORT);
+
+
+async function main() {
+    try {
+    } catch (error) {
+        if (error.response) {
+            console.log("Status:",error.response.status);
+            console.log("Data:",error.response.data);
+        } else {
+            console.log("Message:",error.message);
+        }
+        console.error(error);
+    }
+}
+
+main();
